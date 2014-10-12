9344d5ceb499ace44e8b65cc2824c6cf555fe687
Guillaume Hivert <guillaume.hivert@outlook.com>
2014-10-12 14:41:33 +0200

    Ajoute des fonctions d'harmonisation.

--- harmonisation.ml
+++ harmonisation.ml
@@ -1,46 +1,31 @@
 open Musical_transformations;;
 
-let harmonizeOnTemps obj n tempo apply_fun param_fun =
+let harmonizeOnTemps obj n tempo apply_fun =
   let tmp = ref 0 in
-  let rec loop obj n tempo apply_fun param_fun =
+  let rec loop obj n tempo apply_fun =
     match obj with
-    | Note x -> (tmp := !tmp + duree;
+    | Note (hauteur, volume, duree) -> (tmp := !tmp + duree;
 		 (if ((!tmp - duree) / 1000 * (tempo / 60)) mod n = 0
-		  then (apply_fun (Note x) (match param_fun with
-					    | int -> param_fun
-					    | List -> (alea_liste param_fun))
-		  else Note x))
+		  then (apply_fun (Note (hauteur, volume, duree)))
+		  else Note (hauteur, volume, duree)))
     | Silence duree -> (tmp := !tmp + duree;
 			Silence duree)   
-    | Sequence seq -> (let auw = !tmp and return = Sequence (List.map (fun x -> loop x n tempo) seq) in
+    | Sequence seq -> (let auw = !tmp and return = Sequence (List.map (fun x -> loop x n tempo apply_fun) seq) in
 		       tmp := auw;
 		       return)
-    | Parallel seq -> Parallel (List.map (fun x -> loop x n tempo) seq) in
-  loop obj n tempo liste_accord;;
+    | Parallel seq -> Parallel (List.map (fun x -> loop x n tempo apply_fun) seq) in
+  loop obj n tempo apply_fun;;
   
 (* Prends un morceau obj et transforme la première note de chaque n temps
  * en accord, en choisissant aléatoirement dans liste_accord. *)
 let harmonizeChords obj n tempo liste_accord =
-  harmonizeOnTemps obj n temp chordify (alea_liste liste_accord);;
+  harmonizeOnTemps obj n tempo (fun note -> chordify note (alea_liste liste_accord));;
   
 (* Effectue une harmonisation sur le morceau obj en transformant la
  * première note de chaque n temps en une mélodie avec un décalage
  * decal donné en paramètre. *)
-let harmonizeSequence obj n tempo decal =
-  let tmp = ref 0 in
-  let rec loop obj n tempo decal =
-    match obj with
-    | Note x -> (tmp := !tmp + duree;
-		 (if ((!tmp - duree) / 1000 * (tempo / 60)) mod n = 0
-		  then (sequencify (chordify (Note x) (alea_liste liste_accord)) decal)
-		  else Note x))
-    | Silence duree -> (tmp := !tmp + duree;
-			Silence duree)   
-    | Sequence seq -> (let auw = !tmp and return = Sequence (List.map (fun x -> loop x n tempo) seq) in
-		       tmp := auw;
-		       return)
-    | Parallel seq -> Parallel (List.map (fun x -> loop x n tempo) seq) in
-  loop obj n tempo decal;;
+let harmonizeSequence obj n tempo decal liste_accord =
+  harmonizeOnTemps obj n tempo (fun note -> sequencify (chordify note (alea_liste liste_accord)) decal);;
 
 (* Effectue une harmonisation sur le morceau obj en transformant la
  * première note de chaque n temps en une mélodie avec un décalage
@@ -50,4 +35,8 @@ let harmonizeSequence obj n tempo decal =
  * 0.5 = Harmonisation par accords ou par décalage.
  * 1.0 = Harmonisation par décalage seuls. *)
 let harmonizeRandom obj n tempo decal liste_accord facteur =
-  
+  harmonizeOnTemps obj n tempo (fun note -> Random.self_init;
+					    let rand = Random.float 1.0 in
+					    if rand < facteur
+					    then sequencify (chordify note (alea_liste liste_accord)) decal
+					    else chordify note (alea_liste liste_accord));;
